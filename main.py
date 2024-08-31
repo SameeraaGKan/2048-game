97c0c683f077db5ca147f7e581e794a2cfb3a201
SameeraaGKan <sameeraagk883@gmail.com>
2024-08-31 16:28:57 -0500

    commit

--- main.py
+++ main.py
@@ -27,10 +27,62 @@ MOVE_VELOCITY = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
 
+class Tile:
+    COLORS = [
+        (179, 232, 238), #COLOR FOR 2
+        (133, 224, 235), #COLOR FOR 4
+        (84, 208, 223), #COLOR FOR 8
+        (54, 178, 193), #COLOR FOR 16
+        (37, 151, 165), #COLOR FOR 32
+        (32, 134, 146), #COLOR FOR 64
+        (49, 118, 126), #COLOR FOR 128
+        (14, 75, 82), #COLOR FOR 256
+        (15, 49, 54), #COLOR FOR 512
+        (10, 38, 42), #COLOR FOR 1024
+        (3, 15, 17), #COLOR FOR 2048
+        (7, 54, 32), #COLOR FOR 4096
+    ]
+
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def get_color(self):
+        
+    
+    def draw(self, window):
+        pass
+
+    def set_pos(self):
+        pass
+
+    def move(self, delta):
+        if direction == "up":
+            self.row -= 1
+        elif direction == "down":
+            self.row += 1
+        elif direction == "left":
+            self.col -= 1
+        elif direction == "right":
+            self.col += 1
+
+# create a function to draw the grid lines
+
 def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+            
     pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
     
-
+# function to display the window
 def draw(window):
     window.fill(BACKGROUND_COLOR)
 
